c37e3783f0e779c2a42fe86ead57985e11b8450c
Jillderon <jillderon93@gmail.com>
2016-12-05 13:19:36 +0100

    Initial Commit

---
+++ jillderon-pset6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jillderon-pset6
+//
+//  Created by Jill de Ron on 05-12-16.
+//  Copyright © 2016 Jill de Ron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
